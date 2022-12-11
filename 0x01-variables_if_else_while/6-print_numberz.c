c89186632aee37d66442e2f2df995bdfbfeee210
Mashood <psychokng@gmail.com>
2022-12-11 08:24:52 -0800

    6-print_numberz.c

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int no;
+
+	for (no = 0; no <= 9; no++)
+		putchar(no + '0');
+
+	putchar('\n');
+	return (0);
+
+}
