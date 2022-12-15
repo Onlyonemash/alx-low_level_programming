c2eda0aba5cbba37598a167c1d2312d986fd0be7
Mashood <psychokng@gmail.com>
2022-12-15 02:00:11 -0800

    0-main.c

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -2,16 +2,18 @@
 
 /**
  * main - Test function for positive or negative
+ *
  * Return: 0
  */
 
 int main(void)
+
 {
-	 int i;
+	int i;
 
-	  i = 0;
-	   positive_or_negative(i);
+	i = 0;
+	positive_or_negative(i);
 
-	   return (0);
+	return (0);
+
 }
-
