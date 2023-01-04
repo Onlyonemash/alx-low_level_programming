991da726ff05720f58ac27cb241da8410dc4e911
Mashood <psychokng@gmail.com>
2023-01-04 15:46:27 -0800

    101-wildcmp.c

--- 0x08-recursion/101-wildcmp.c
+++ 0x08-recursion/101-wildcmp.c
@@ -50,9 +50,9 @@ void iterate_wild(char **wildstr)
  *    * @str: The string to be matched.
  *     * @postfix: The postfix.
  *      *
- *       * Return: If str and postfix are identical - a pointer to the null byte
- *        *                                            located at the end of postfix.
- *         *         Otherwise - a pointer to the first unmatched character in postfix.
+ *  Return: If str and postfix are identical - a pointer to the null byte
+ *  located at the end of postfix.
+ *  Otherwise - a pointer to the first unmatched character in postfix.
  */
 
 char *postfix_match(char *str, char *postfix)
